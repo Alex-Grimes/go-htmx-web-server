5ce71b25d361a563440e0df884f17e9190c0c77e
alex <alexgrimes.se@gmail.com>
2023-09-09 21:52:12 -0400

    basic template improvments

--- main.go
+++ main.go
@@ -7,13 +7,28 @@ import (
 	"text/template"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 
 	fmt.Println("server is running ...")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "Casablanca", Director: "Michael Curtiz"},
+				{Title: "Cool Hand Luke", Director: "Stuart Rosenberg"},
+				{Title: "Bullitt", Director: "Peter Yates"},
+				{Title: "The French Connection", Director: "William Friedkin"},
+			},
+		}
+
+		tmpl.Execute(w, films)
 	}
 	http.HandleFunc("/", h1)
 
